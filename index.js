109e77f3a414786282a75d8463c0ad35a5e4737e
“nrromie” <nrromie@gmail.com>
2023-11-27 12:46:37 +0600

    Implemented posting user data

--- index.js
+++ index.js
@@ -100,6 +100,24 @@ async function run() {
             }
         });
 
+        //Adding User Info
+        app.post('/users', async (req, res) => {
+            const user = req.body;
+            const query = { email: user.email }
+            const existingUser = await userCollection.findOne(query);
+            if (existingUser) {
+                return res.send({ message: 'user already exists', insertedId: null })
+            }
+            const result = await userCollection.insertOne(user);
+            res.send(result);
+        });
+
+        app.get('/users', async (req, res) => {
+            const users = await userCollection.find().toArray();
+            res.send(users);
+        })
+
+
 
 
     } finally {
